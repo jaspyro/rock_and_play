ea571cff150f40f72d8cc185ee9c35e910f102e1
jaspyro <martin.baudry@sfr.fr>
2022-06-08 13:39:07 +0200

    commit script.js

--- projet_html_code/script.js
+++ projet_html_code/script.js
@@ -2,7 +2,6 @@
 rules = document.querySelector('.rules-btn');
 aside = document.querySelector('aside');
 closeRules = document.querySelector('.close-rules');
-console.log(closeRules);
 
 rules.addEventListener('click', event => {
 
@@ -19,3 +18,55 @@ closeRules.addEventListener('click', event => {
 
 
 });
+
+section = document.querySelector('section');
+step_1 = document.querySelector('.step-1');
+choix = document.querySelectorAll('.option-container');
+
+paper = document.querySelector('.paper');
+scissors = document.querySelector('.scissors');
+rock = document.querySelector('.rock');
+
+console.log(paper);
+console.log(rock);
+console.log(scissors);
+
+step_1.addEventListener('click', event => {
+   event.preventDefault();
+   step_1.classList.add('.step-1-minimize');
+})
+
+// item.addEventListener('click', event => {
+//     event.preventDefault();
+//     if ( item == 2 )  {
+//        console.log("paper");
+//     } else if ( choix == rock ) {
+//        console.log("rock")
+//     } else {
+//        console.log("scissors");
+//    }
+//  });
+
+// choix.forEach(index => {
+//       console.log(index);
+//       if ( choix.lenght == 0 ) {
+//          console.log("papper");
+//       } else if ( choix == rock ) {
+//          console.log("rock");
+//       } else if ( choix == scissors ) {
+//          console.log("scissors");
+//       } else {
+//          console.log("unknow");
+//       }
+// });
+
+// choix.('click', event => {
+//    event.preventDefault();
+//    if ( choix == paper )  {
+//       console.log("paper");
+//    } else if ( choix == rock ) {
+//       console.log("rock")
+//    } else {
+//       console.log("scissors");
+//    }
+// });
