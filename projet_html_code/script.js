a3032380becf8ea111b615c8529af5024bfd5da3
jaspyro <martin.baudry@sfr.fr>
2022-06-09 16:31:28 +0200

    add new function but bug detected

--- projet_html_code/script.js
+++ projet_html_code/script.js
@@ -1,7 +1,53 @@
-//Recuperer var rules
+// show rules
 rules = document.querySelector('.rules-btn');
 aside = document.querySelector('aside');
 closeRules = document.querySelector('.close-rules');
+
+// retrieve element
+step_1 = document.querySelector('.step-1');
+step_2 = document.querySelector('.step-2');
+
+player_pick = document.querySelector('.player-pick');
+computer_pick = document.querySelector('.step-2 .computer-pick');
+console.log(computer_pick);
+
+replace = document.querySelector('.replace');
+choix = document.querySelector('.option-container');
+option = computer_pick.querySelector('.option-container');
+console.log(option);
+btn_play_again = document.querySelector('.play-again-btn');
+
+// replace winner 
+div = document.querySelector('.annouce-winner');
+winner = document.querySelector('.annouce-winner-text');
+new_p = document.createElement('P');
+you_lose = document.createTextNode("YOU LOSE");
+no_winner = document.createTextNode("NO WINNER");
+
+// player choice
+paper = document.querySelector('.paper');
+scissors = document.querySelector('.scissors');
+rock = document.querySelector('.rock');
+
+// computer jeux
+computer_paper = computer_pick.paper;
+computer_scissors = computer_pick.scissors;
+computer_rock = computer_pick.rock;
+
+
+// computer choice
+paper_prime = paper.cloneNode(true);
+scissors_prime = scissors.cloneNode(true);
+rock_prime = rock.cloneNode(true);
+
+
+// initialisation
+main = "";
+nbr = "";
+element = "";
+nbr2 = "";
+element2 = "";
+
 
 rules.addEventListener('click', event => {
 
@@ -14,65 +60,75 @@ rules.addEventListener('click', event => {
 closeRules.addEventListener('click', event => {
 
    event.preventDefault();
-   aside.style.opacity = "0"; 
+   aside.style.opacity = "0";
+
 });
 
 //reste fond à assombrir et modifier taille de l'aside
 
-//**********************STEP-1******************* */
-
-
-section = document.querySelector('section');
-step_1 = document.querySelector('.step-1');
-step_2 = document.querySelector('.step-2');
-player_pick = document.querySelector('.player-pick');
-computer_pick = document.querySelector('.computer-pick');
-replace = document.querySelector('.replace');
-choix = document.querySelectorAll('.option-container');
-
-
-paper = document.querySelector('.paper');
-scissors = document.querySelector('.scissors');
-rock = document.querySelector('.rock');
-
-console.log(section);
-console.log(step_2);
-
-function random() {
+// function random
+function randoms() {
    random = Math.floor(Math.random() * 3);
    if (random == 0) {
-      computer_pick.appendChild(paper);
+
+      console.log("paper ok");
+      return option.replaceWith(paper_prime);
    } else if (random == 1) {
-      computer_pick.appendChild(scissors);
+
+      console.log("scisseux ok");
+      return option.replaceWith(scissors_prime);
    }  else {
-      computer_pick.appendChild(rock);
+
+      console.log("rock ok");
+      return option.replaceWith(rock_prime);  
    }
 }
 
-paper.addEventListener('click', event => {
-   event.preventDefault();
-   step_1.classList.add('step-1-minimize');
-   step_2.classList.add('step-2-minimizer');
-   player_pick.appendChild(paper);
-   
-   random();
-})
+// function if random number is 
+function ifRandom(nbr, element) {
+   if (random == nbr ) {
 
-scissors.addEventListener('click', event => {
-   event.preventDefault();
-   step_1.classList.add('step-1-minimize');
-   step_2.classList.add('step-2-minimizer');
-   player_pick.appendChild(scissors);
+      winner.replaceWith(new_p);
+      new_p.classList.add('annouce-winner-text');
+      div.nextSibling.new_p;
+      new_p.appendChild(element)
+   }
+}
 
-   random();
-})
+// function btn play again
+function play_again (main) {
+   btn_play_again.addEventListener('click', event => {
+      event.preventDefault();
+      console.log(option);
+      step_2.classList.remove('step-2-minimizer');
+      step_1.classList.remove('step-1-minimize');
+      test2 = querySelector('.step-2 .computer_pick');
+      test = test2.querySelector('.option-container.replace');
+      test2.removeChild(test);
+      
+      // step_1.appendChild(main);
+   })
+}
 
-rock.addEventListener('click', event => {
-   event.preventDefault();
-   step_1.classList.add('step-1-minimize');
-   step_2.classList.add('step-2-minimizer');
-   player_pick.appendChild(rock);
 
-   random();
-})
 
+// main function
+function EventListener (main, nbr, element, nbr2, element2) {
+   main.addEventListener('click', event => {
+      event.preventDefault();
+      step_1.classList.add('step-1-minimize');
+      step_2.classList.add('step-2-minimizer');
+      player_pick.append(main);
+
+      randoms();
+
+      ifRandom(nbr, element);
+      ifRandom(nbr2, element2);
+
+      play_again(main);  
+   })
+}
+
+EventListener(paper,1,you_lose,0,no_winner);
+EventListener(scissors,2,you_lose,1,no_winner);
+EventListener(rock,0,you_lose,2,no_winner);
